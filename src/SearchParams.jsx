bf6e7f957ddbe5cb97be6bbf9ef6c4d6c0726d8c
tony <anthonymarques@outlook.com>
2023-10-24 09:30:26 +0200

    React Search Params in progress

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,23 +1,61 @@
 import { useState } from "react";
+const ANIMALS = ['bird', "cat", "dog", "rabbit","shark"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("");
-    return (
-        <div className="search-params">
-            <form>
-                <label htmlFor="Location">
-                    Locartion
-                    <input
-                        onChange={(e) => setLocation(e.target.value)}
-                        id="location"
-                        value={location} 
-                        placeholder="Location" />
-                </label>
-                <button>Submit</button>
-            </form>
-
-        </div>
-    )
-}
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["mixed", "Dog"]
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="Location">
+          Locartion
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            placeholder="animal"
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+               return <option key={animal}>{animal}</option>
+            })}
+            <option />
+          </select>
+        </label>
+        <label htmlFor="breed">
+          breed
+          <select
+            id="breed"
+            value={breed}
+            placeholder="breed"
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => {
+               return <option key={breed}>{breed}</option>
+            })}
+            <option />
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SearchParams;
